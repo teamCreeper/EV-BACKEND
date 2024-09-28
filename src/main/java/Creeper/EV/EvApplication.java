a539411205a967db20a3b31cfda7230c4abdf541
KimSnowFlower <kimbona1148@gmail.com>
2024-09-28 09:42:58 +0900

    create_search_value

--- src/main/java/Creeper/EV/EvApplication.java
+++ src/main/java/Creeper/EV/EvApplication.java
@@ -2,6 +2,9 @@ package Creeper.EV;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EvApplication {
@@ -10,4 +13,15 @@ public class EvApplication {
 		SpringApplication.run(EvApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
--- src/main/java/Creeper/EV/Repository/InfoRepository.java
+++ src/main/java/Creeper/EV/Repository/InfoRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface InfoRepository extends JpaRepository<InfoEntity, Long> {
     InfoEntity findByBrandAndName(String brand, String name); // 필드와 DB 컬럼을 맞춤
+    InfoEntity findByName(String name); 
 }
