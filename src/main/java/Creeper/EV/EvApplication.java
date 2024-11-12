da80248a0eaf2b5093551a9c52f2ba276bb27f2e
KimSnowFlower <kimbona1148@gmail.com>
2024-11-12 09:44:54 +0900

    43/chore/build_EV-0.1.1

--- src/main/java/Creeper/EV/EvApplication.java
+++ src/main/java/Creeper/EV/EvApplication.java
@@ -19,7 +19,7 @@ public class EvApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://o11117.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
