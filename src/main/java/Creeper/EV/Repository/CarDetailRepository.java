d0a177508a0700af5762f49377c9795ca8c04c9e
KimSnowFlower <kimbona1148@gmail.com>
2024-11-24 11:26:20 +0900

    fix/carDetail_logic

--- src/main/java/Creeper/EV/Repository/CarDetailRepository.java
+++ src/main/java/Creeper/EV/Repository/CarDetailRepository.java
@@ -16,13 +16,6 @@ public interface CarDetailRepository extends JpaRepository<CarDetailInfo, Long> 
     @Query("SELECT new Creeper.EV.DTO.CarDetailDTO(c.carId, b.batteryId, c.carName, batteryType, b.capacity, b.charge_time, br.brandName, br.brandCountry) " +
     "FROM CarDetailInfo d JOIN d.carBasicInfo c JOIN d.batteryInfo b JOIN b.batteryBrand br ")
     List<CarDetailDTO> findByCarInfo();
-<<<<<<< Updated upstream
-
-    // 메인 페이지 조회용
-    @Query("SELECT b.batteryId FROM CarDetailInfo d JOIN d.batteryInfo b WHERE d.carBasicInfo.carId = :carId")
-    String findBatteryIdByCarId(@Param("carId") Long carId);
-=======
->>>>>>> Stashed changes
     
     List<CarDetailInfo> findByCarId(Long carId);
 }
